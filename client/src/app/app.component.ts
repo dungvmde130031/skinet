4f1e1f7a3be1ded2406c245c40b7a1b7554b37a3
toi.vomanhdung@gmail.com <toi.vomanhdung@gmail.com>
2024-02-23 10:35:50 +0700

    End of section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,3 @@
-import { Pagination } from './models/pagination';
-import { Product } from './models/product';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,18 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: Product[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products').subscribe({
-      next: response => this.products = response.data,
-      error: error => console.log(error),
-      complete: () => {
-        console.log('request completed');
-        console.log('extra statement');
-      }
-    })
+    
   }
 }
